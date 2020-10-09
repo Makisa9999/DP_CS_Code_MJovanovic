8f7aa50b9bfa2942e595ac42fb874ed4c53c4555
Mario Jovanovic <mario.jovanovic@FVFYLWQTJ1WK.local>
2020-10-09 00:22:40 -0400

    October 9th 2020

---
+++ tools/findIntervalSum.py
@@ -0,0 +1,17 @@
+def findIntervalSum (a, b):
+    if a < b:
+        minimum = a
+        maximum = b
+        suma = 0
+        for i in range(minimum, maximum + 1, 1):
+            suma = suma + i
+        return suma
+    else:
+        minimum = b
+        maximum = a
+        suma = 0
+        for i in range(minimum, maximum + 1, 1):
+            suma = suma + i
+        return suma
+
+print(findIntervalSum(12,15))
