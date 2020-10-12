9fec87b5dd05524abc529e524fb94696d6e379de
Mario Jovanovic <mario.jovanovic@FVFYLWQTJ1WK.local>
2020-10-12 13:55:04 -0400

    October 12th 2020

---
+++ Programming/Python8.py
@@ -0,0 +1,14 @@
+import turtle
+
+'''
+wn = turtle.Screen() #This will create a window where turtle can move.
+alex = turtle.Turtle() #This will set alex to change turtle.Turtle() so we don't have to write it
+wn.bgcolor("color") #This will set background color of the canvas to color
+alex.left(number) #This will rotate turtle for number degrees to the left
+alex.right(number) #This will rotate turtle for number degrees to the right
+alex.forward(number) #This will move the turtle for number of units forward
+alex.color(color) #This will make lines that turtle makes color put in brackets
+wn.exitonclick() #When you click on canvas, it will leave and delete it
+alex.pensize(number) #Will set a pensize to a number in brackets
+'''
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DP_CS_Code_MJovanovic
 Diploma Program Computer Science Repository
+Here i will be doing all the tasks I need to do for my Diploma Program Computer Science Course
